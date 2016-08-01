48c67a9bc6c7db8e41026a5de0548e88fb1aec66
AbhishekGahlot <me@abhishek.it>
2016-08-01 09:03:09 +0530

    Fixed whitespace

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from sh import sudo
 
 ### TCP Slow Start Optimization
 sysctl = sh.Command('sysctl')
-tcp_slow_start = sysctl('net.ipv4.tcp_slow_start_after_idle').strip()
-if tcp_slow_start == 'net.ipv4.tcp_slow_start_after_idle = 1':
+tcp_slow_start = sysctl('net.ipv4.tcp_slow_start_after_idle').strip().replace(' ','')
+enabled_tcp_slow_start = 'net.ipv4.tcp_slow_start_after_idle=1'
+disabled_tcp_slow_start = 'net.ipv4.tcp_slow_start_after_idle=0'
+if tcp_slow_start == enabled_tcp_slow_start:
   print 'Optimizing TCP Slow Start'
-  sudo('sysctl', '-w', 'net.ipv4.tcp_slow_start_after_idle=0')
-elif tcp_slow_start == 'net.ipv4.tcp_slow_start_after_idle = 0':
+  sudo('sysctl', '-w', disabled_tcp_slow_start)
+elif tcp_slow_start == disabled_tcp_slow_start:
   print 'TCP Slow Start already optimized'
 else:
   print 'Error Occurred with Slow Start Optimization'
